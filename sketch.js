f085fecedf273bcf0035389442ea42a11d2401e8
allisonoh5 <123608124+allisonoh5@users.noreply.github.com>
2023-10-25 22:23:20 -0400

    Translation Visualization

--- sketch.js
+++ sketch.js
@@ -1,47 +1,66 @@
-let hourAngle, minuteAngle, secondSize;
+let countries;
+let selectedCountry;
+let phrases = ["Hello", "Thank you", "Please", "Yes", "No", "I need help", "How much?", "Where is ...?"];
+let translations = {};
 
+//data loading
+function preload() {
+  countries = loadJSON("https://restcountries.com/v3.1/all");
+  loadJSON("https://restcountries.com/v3.1/all", function(data) {
+  countries = data;
+  console.log(countries);
+  });
+}
+
+//
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  angleMode(DEGREES);
-  noStroke();
-}
+  createCanvas(800, 600);
+  background(245, 245, 245);
 
-function draw() {
-  background(245);
+  let dropdown = createSelect();
+  dropdown.position(50, 50);
+  dropdown.option("Select a country");
 
-  let hours = hour() % 12;  // 0-11
-  let minutes = minute();   // 0-59
-  let seconds = second();   // 0-59
-
-  translate(width / 2, height / 2);
-
-  // Hours as rotating concentric circles
-  for (let i = 1; i <= hours; i++) {
-    let radius = i * 20; // space out the circles
-    let offsetAngle = 360 * (i / 12); // each circle rotates by a different amount
-    let x = radius * cos((hourAngle + offsetAngle) % 360);
-    let y = radius * sin((hourAngle + offsetAngle) % 360);
-    fill(50, 50 + i * 10, 200 - i * 10);  // changing color for each circle
-    ellipse(x, y, 15, 15);
+  for (let country of countries) {
+    dropdown.option(country.name.common);
   }
 
-  // Minutes as radiating lines
-  stroke(0, 150, 0);
-  strokeWeight(2);
-  let spacing = 360 / 60;  // each line is spaced by 6 degrees
-  for (let i = 0; i < minutes; i++) {
-    let x = 150 * cos(i * spacing);
-    let y = 150 * sin(i * spacing);
-    line(0, 0, x, y);
-  }
-
-  // Seconds as a pulsing circle
-  let pulseSize = map(seconds, 0, 59, 10, 50);
-  fill(255, 0, 0, 150);  // semi-transparent red
-  ellipse(0, 0, pulseSize, pulseSize);
-
-  // Update the angles
-  hourAngle = map(hours, 0, 12, 0, 360) + map(minutes, 0, 60, 0, 30); // the hour circle will move half a degree each minute
-  minuteAngle = map(minutes, 0, 60, 0, 360);
+  dropdown.changed(() => {
+    selectedCountry = dropdown.value();
+    getTranslations(selectedCountry);
+  });
 }
+
+//fetch the translations from the data
+
+function getTranslations(countryName) {
+  let country = countries.find(c => c.name.common === countryName);
+  let languageCode = Object.keys(country.languages)[0]; // Get the language
+  
+  for (let phrase of phrases) {
+    let url = `https://api.mymemory.translated.net/get?q=${phrase}&langpair=en|${languageCode}`;
+    loadJSON(url, data => {
+      translations[phrase] = data.responseData.translatedText;
+      drawTranslations();
+    });
+  }
+}
+
+//draw the translations
+function drawTranslations() {
+  background(245, 245, 245);
+  fill(50);
+  textSize(24);
+  text(selectedCountry, 50, 100);
+
+  let y = 150;
+  for (let phrase in translations) {
+    textSize(18);
+    text(phrase + ": ", 50, y);
+    text(translations[phrase], 300, y);
+    y += 30;
+  }
+}
+
+function draw() {} // Empty draw function bc doesn't need to be updated
 
