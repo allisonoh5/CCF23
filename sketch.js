41f282764aa4e5f793871537eb4f8fc1d5138917
allisonoh5 <123608124+allisonoh5@users.noreply.github.com>
2023-11-08 23:27:42 -0500

    Modified Clock

--- sketch.js
+++ sketch.js
@@ -1,100 +1,58 @@
-// Classifier Variable
-let classifier;
-
-// Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/_81zbs4jl/';
-
-// Video
-let video;
-let flippedVideo;
-
-// To store the classification
-let label = "";
-
-// Load the model first
-function preload() {
-  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
-}
+let orb;  // stores vector
 
 function setup() {
-  // Create a larger canvas than the video size
-  createCanvas(640, 480);
-  // Create the video
-  video = createCapture(VIDEO);
-  video.size(320, 240);
-  video.hide();
-
-  flippedVideo = ml5.flipImage(video);
-  // Start classifying
-  classifyVideo();
+  createCanvas(windowWidth, windowHeight);
+  orb = createVector(width / 2, height / 2);  // Initialize orb position as a vector
 }
 
 function draw() {
-  background(0);
-  // Draw the video in the center
-  image(flippedVideo, (width - video.width) / 2, (height - video.height) / 2);
+  let currentHour = hour();  // Get current hour
 
-  // Provide feedback based on the label
-  if (label === "Face not clearly detected") {
-    fill(255, 0, 0); // Red text
-    textSize(16);
-    text(label, width / 2, height - 50); 
-    // Display above video
+  // Define emotion based on the hour
+  let emotion;
+  if (currentHour >= 6 && currentHour < 12 || currentHour >= 20 && currentHour < 24) {
+    emotion = "calm";
+  } else if (currentHour >= 12 && currentHour < 16) {
+    emotion = "euphoria";
   } else {
-    // Draw a background box for label
-    fill(0, 102, 153, 200); // Semi-transparent background box
-    noStroke();
-    rect((width - video.width) / 2, height - 35, video.width, 24);
-
-    // Draw the label
-    fill(255); // White text
-    textSize(16);
-    textAlign(CENTER);
-    text(label, width / 2, height - 16);
+    emotion = "melancholy";
   }
 
-  // feedback based on label
-  textSize(32);
-  textAlign(CENTER);
-  if (label == "Your mouth is closed") {
-    fill(255);
-    text("😐", width / 2, 100); // Positioned above the video
+  // Adjust visuals based on the emotion
+  switch (emotion) {
+    case "calm":
+      background(135, 206, 235);  // Soft blue
+      drawWaves(5, 0.005);        // Slow waves
+      orb.y = lerp(orb.y, height / 2, 0.05); // modified
+      break;
+    case "euphoria":
+      background(255, 255, 51);   // Bright yellow
+      drawWaves(20, 0.03);        // Fast waves
+      orb.y = lerp(orb.y, height * 0.25, 0.05); // modified
+      break;
+    case "melancholy":
+      background(138, 43, 226);   // Deep purple
+      drawWaves(20, 0.01);        // Moderate waves
+      orb.y = lerp(orb.y, height * 0.75, 0.05); // modified
+      break;
   }
-  else if (label == "Your mouth is open") {
-    fill(255);
-    text("😮", width / 2, 100); // Positioned above the video
-  }
-  else if (label == "Where are you?") {
-    fill(255);
-    text("🤔", width / 2, 100); // Positioned above the video
-  }
+
+  // Draw the floating orb
+  fill(255);
+  noStroke();
+  ellipse(orb.x, orb.y, 50, 50); //modified
 }
 
-// Get a prediction for the current video frame
-function classifyVideo() {
-  flippedVideo = ml5.flipImage(video);
-  classifier.classify(flippedVideo, gotResult);
-}
-
-// When we get a result
-function gotResult(error, results) {
-  // If there is an error
-  if (error) {
-    console.error(error);
-    return;
+function drawWaves(numWaves, waveSpeed) {
+  stroke(255);
+  noFill();
+  for (let i = 0; i < numWaves; i++) {
+    let y = map(i, 0, numWaves, 0, height);
+    beginShape();
+    for (let x = 0; x < width; x += 10) {
+      let yOffset = sin((x * waveSpeed) + (frameCount * waveSpeed * 2)) * 30;
+      vertex(x, y + yOffset);
+    }
+    endShape();
   }
-  // The results are in an array ordered by confidence.
-  label = results[0].label;
-  let confidence = results[0].confidence;
-
-  // Log the label and confidence for debugging
-  console.log(label, confidence);
-
-  // Define a threshold for confidence
-  if (confidence < 0.75) {
-    label = "Face not clearly detected";
-  }
-
-  // Classify again
-  classifyVideo();
 }
